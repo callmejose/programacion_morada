e99f852ad114e229f053a391a03186ff939e4719
ojosedo <ojosedo@gmail.com>
2014-04-11 20:39:47 -0500

    Primeros códigos en python

---
+++ Python/hola_mundo.py
@@ -0,0 +1,3 @@
+# este programa dice 'hola mundo'
+
+print('hola mundo') #imprime lo que esta dentro de los paréntesis
---
+++ Python/saludo.py
@@ -0,0 +1,5 @@
+# este programa te saluda
+
+print('hola, cuál es tu nombre?') #impresión
+mi_nombre = input() #la función "input" lee lo que el usuario escriba, luego esto se lleva a la variable "mi_nombre"
+print('hola ' + mi_nombre + ', es un placer conocerte') #saluda al usuario
