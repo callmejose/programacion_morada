1862eee961566f694ddb360b2b3ebc760dd573ee
ojosedo <ojosedo@gmail.com>
2014-08-12 06:27:22 -0500

    comentarios ejemplo de objetos

--- Python/ejemplos/objetos/punto.py
+++ Python/ejemplos/objetos/punto.py
@@ -1,47 +1,49 @@
-class Punto(object):
+class Punto(object): #esta es la definición de la clase Punto, "object" es una palabra reservada de python
     """Representa un punto"""
 
 print(Punto)
 
-blanco = Punto()
+blanco = Punto() #creo un objeto de tipo Punto
 
 print(blanco)
 
 ###atributos
 
-blanco.x = 3
+blanco.x = 3 #dentro del objeto "blanco" creo el atributo "x" y lo hago igual a 3
 blanco.y = 4
 
-print(blanco.x)
+print(blanco.x) #¿qué crees que se imprima?
 
-def print_punto(p):
+def print_punto(p): #creo una función a la que le entra un objeto tipo punto
     print("(%.1f, %.1f)" %(p.x, p.y))
     
-print_punto(blanco)
+print_punto(blanco) #utilizo la función con el objeto que antes habia creado
 
 
-class Rectangulo(object):
+class Rectangulo(object): #defino una nueva clase
     """Representa un rectangulo.
     
-    attributes: ancho, alto, esquina.
+    atributos: ancho, alto, esquina.
     """
 
-caja = Rectangulo()
-caja.ancho = 100
-caja.alto = 200
+caja = Rectangulo() #creo un objeto tipo Rectangulo, este objeto se llama "caja"
+caja.ancho = 100 #dentro del objeto caja creo una variable llamada "ancho" y la hago 100
+caja.alto = 200 #lo mismo para "alto"
 
-caja.esquina = Punto()
-caja.esquina.x = 0
+caja.esquina = Punto() #dentro del objeto caja creo un objeto de tipo Punto, este nuevo objeto se llama "esquina"
+caja.esquina.x = 0 #a ese objeto que acabo de crear dentro de "caja" le creo una variable "x"
 caja.esquina.y = 0
 
-def encontrar_centro(rect):
+def encontrar_centro(rect): #definición de función a la que le entra un objeto tipo Rectangulo y retorna un objeto tipo Punto
     p = Punto()
     p.x = rect.esquina.x + rect.ancho/2
     p.y = rect.esquina.y + rect.alto/2.0
     return p
     
-centro = encontrar_centro(caja)
+centro = encontrar_centro(caja) #utilizo la función con el objeto "caja" que habia creado antes
 print_punto(centro)
+
+###copia de objetos
 
 caja2 = caja
 
