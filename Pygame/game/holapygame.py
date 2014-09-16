0b61c62703374f11c856545721f085e0b6ad86ae
ojosedo <ojosedo@gmail.com>
2014-09-16 00:12:19 -0500

    juego actualizado

--- Pygame/game/holapygame.py
+++ Pygame/game/holapygame.py
@@ -6,37 +6,44 @@ NEGRO = [ 0, 0, 0]
 BLANCO = [ 255, 255, 255]
 VERDE = [ 0, 255, 0]
 ROJO = [ 255, 0, 0]
-AZUL = [ 0, 0, 255]
-# Constantes en mayúsculas
 
-def dubujar_personita(pantalla, x, y):
-	# Cabeza
-	pygame.draw.ellipse(pantalla, NEGRO,[96+x,83+y,10,10],0)
-	# Piernas
-	pygame.draw.line(pantalla, NEGRO, [100+x,100+y], [105+x,110+y], 2)
-	pygame.draw.line(pantalla, NEGRO, [100+x,100+y], [95+x,110+y], 2)
-	# Cuerpo
-	pygame.draw.line(pantalla, ROJO, [100+x,100+y], [100+x,90+y], 2)
-	# Brazos
-	pygame.draw.line(pantalla, ROJO, [100+x,90+y], [104+x,100+y], 2)
-	pygame.draw.line(pantalla, ROJO, [100+x,90+y], [96+x,100+y], 2)
+# Constantes en mayúsculas
 
 # Inicializa el motor de juegos
 pygame.init()
-
-dimensiones = [400, 400]
+h=256
+g=256
+dimensiones = [h, g]
 # Abrir la pantalla (otra opción es open_window)
 pantalla = pygame.display.set_mode(dimensiones)
+
+pasto = pygame.image.load("grass.png").convert()
+avatar = pygame.image.load("james.png").convert()
+avatar.set_colorkey(NEGRO)
+
+musica = pygame.mixer.Sound("Collision.ogg")
 
 pygame.display.set_caption("Mi primer juego")
 
 # ESTE ES EL BUCLE PRINCIPAL DEL PROGRAMA
-
+pygame.mouse.set_visible(False)
 #Itera hasta que el usuario pincha sobre el botón de cierre.
 hecho = False
 
 # Se usa para gestionar cuan rápido se actualiza la pantalla
 reloj = pygame.time.Clock()
+y = -128
+x = -128
+velocidad_x=0
+velocidad_y=0
+def barquito(x, y):
+    pygame.draw.line(pantalla, NEGRO,[x+200, y+0], [x+200, y+200], 2)
+    pygame.draw.line(pantalla, NEGRO,[x+200, y+0], [x+300, y+50])
+    pygame.draw.line(pantalla, NEGRO,[x+200, y+100], [x+300, y+50])
+    pygame.draw.line(pantalla, NEGRO,[x+0, y+200], [x+400, y+200])
+    pygame.draw.line(pantalla, NEGRO,[x+0, y+200], [x+100, y+400])
+    pygame.draw.line(pantalla, NEGRO,[x+100, y+400], [x+300, y+400])
+    pygame.draw.line(pantalla, NEGRO,[x+300, y+400], [x+400, y+200])
 
 # -------- Bucle Principal del Programa -----------
 while not hecho:
@@ -44,6 +51,26 @@ while not hecho:
     for evento in pygame.event.get(): # El usuario hizo algo
         if evento.type == pygame.QUIT: # Si el usuario pincha sobre cerrar
             hecho = True # Marca que indica que hemos acabado y sale de este bucle
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            pass
+        if evento.type==pygame.KEYDOWN:
+            if evento.key==pygame.K_UP:
+                velocidad_y=-2
+            if evento.key==pygame.K_DOWN:
+                velocidad_y=2
+            if evento.key==pygame.K_RIGHT:
+                velocidad_x=2
+            if evento.key==pygame.K_LEFT:
+                velocidad_x=-2
+        if evento.type==pygame.KEYUP:
+            if evento.key==pygame.K_UP:
+                velocidad_y=0
+            if evento.key==pygame.K_DOWN:
+                velocidad_y=0
+            if evento.key==pygame.K_RIGHT:
+                velocidad_x=0
+            if evento.key==pygame.K_LEFT:
+                velocidad_x=0
             
     # TODOS LOS EVENTOS DE PROCESAMIENTO DEBERÍAN IR ENCIMA DE ESTE COMENTARIO
     
@@ -51,33 +78,26 @@ while not hecho:
     # TODA LA LÓGICA DEL JUEGO DEBERÍA IR DEBAJO DE ESTE COMENTARIO
     
     # TODA LA LÓGICA DEL JUEGO DEBERÍA IR ENCIMA DE ESTE COMENTARIO
+
+    x+=velocidad_x
+    y+=velocidad_y
     
+    pos = pygame.mouse.get_pos()
+    xm = pos[0]
+    ym = pos[1]
     
     # TODO EL CÓDIGO DE DIBUJO DEBERÍA IR DEBAJO DE ESTE COMENTARIO
-
+   
+    
     # Primero limpiamos pantalla. No dibujes por encima de esta linea
     # o todo lo que escribas sera borrado por este comando.
     pantalla.fill(BLANCO)
+    pantalla.blit(pasto, [x, y])
+    pantalla.blit(avatar, [xm, ym])
+    
     
     # DIBUJEMOS ALGUNAS FIGURAS
-    pygame.draw.line(pantalla, VERDE, [15, 15], [30, 30], 5)
-    pygame.draw.line(pantalla, VERDE, [15, 30], [30, 15], 5)
-    pygame.draw.rect(pantalla, NEGRO, [150, 10, 50, 20], 2)
-    pygame.draw.ellipse(pantalla, ROJO, [225, 10, 50, 20])
-    pygame.draw.rect(pantalla, NEGRO, [ 350, 10, 49, 49])
-    
-    pos = pygame.mouse.get_pos()
-    x = pos[0]
-    y = pos[1]
-    dubujar_personita(pantalla, x, y)
-    
-    # DIBUJEMOS ALGUN TEXTO
-    # Seleccionamos la fuente, tamaño, negrita, acostada
-    fuente = pygame.font.SysFont('Calibri', 25, True, False) 
-    # Rendirazar, mi texto, suavizado, color
-    texto = fuente.render("Este es mi texto", True, NEGRO) 
-    # Poner en pantalla el texto
-    pantalla.blit(texto, [250, 250])
+    # barquito(x-200,y-200)
     
     # Avanza y actualiza la pantalla con lo que hemos dibujado.
     pygame.display.flip()  
---
+++ Pygame/juego/main.py
@@ -0,0 +1,152 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Importa la librería de funciones llamada 'pygame'
+import pygame
+
+# Definimos algunos colores
+NEGRO = [ 0, 0, 0]
+BLANCO = [ 255, 255, 255]
+VERDE = [ 0, 255, 0]
+ROJO = [ 255, 0, 0]
+
+# Constantes en mayúsculas
+
+# Inicializa el motor de juegos
+pygame.init()
+h=32*20
+g=32*16
+dimensiones = [h, g]
+# Abrir la pantalla (otra opción es open_window)
+pantalla = pygame.display.set_mode(dimensiones)
+
+pygame.display.set_caption("Mi primer juego")
+
+# ESTE ES EL BUCLE PRINCIPAL DEL PROGRAMA
+pygame.mouse.set_visible(False)
+#Itera hasta que el usuario pincha sobre el botón de cierre.
+hecho = False
+
+# Se usa para gestionar cuan rápido se actualiza la pantalla
+reloj = pygame.time.Clock()
+y = -128
+x = -128
+velocidad_x=0
+velocidad_y=0
+
+class SpriteSheet(object):
+    """ Clase usada para sacar imágenes de la hoja de sprites."""
+    # inicializamos la hoja de sprites
+    sprite_sheet = None
+
+    def __init__(self, file_name):
+        """ Constructor. Le entra el nombre de archivo de la hoja de sprites. """
+
+        # Carga la hoja de datos completa.
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+
+
+    def get_image(self, fila, columna, ancho, alto):
+        """ retorna una única imagen
+        se le pasa la posición (x, y)
+        y el ancho y alto del sprite. """
+
+        # Crea una imagen en blanco
+        imagen = pygame.Surface([ancho, alto]).convert()
+
+        # Copia una parte de la hoja de sprites a la imagen
+        imagen.blit(self.sprite_sheet, (0, 0), (fila * ancho, columna * alto, ancho, alto))
+
+        # Asume que el negro es el color transparente
+        imagen.set_colorkey(NEGRO)
+
+        # Retorna la imagen
+        return imagen
+        
+hoja_completa = SpriteSheet("./imagenes/completa32.png")
+
+principal = hoja_completa.get_image(1, 8, 32, 32)
+pasto = hoja_completa.get_image(14, 1, 32, 32)
+agua = hoja_completa.get_image(13, 7, 32, 32)
+sombra_agua = hoja_completa.get_image(14, 7, 32, 32)
+
+def dibujar_fondo():
+    pantalla.blit(pasto, [0*32, 0*32])
+    pantalla.blit(pasto, [1*32, 0*32])
+    pantalla.blit(pasto, [0*32, 1*32])
+    pantalla.blit(pasto, [1*32, 1*32])
+    pantalla.blit(pasto, [1*32, 2*32])
+    pantalla.blit(pasto, [2*32, 2*32])
+    pantalla.blit(pasto, [1*32, 3*32])
+    pantalla.blit(pasto, [2*32, 3*32])
+    pantalla.blit(agua, [3*32, 0*32])
+    pantalla.blit(sombra_agua, [3*32, 0*32])
+        
+# -------- Bucle Principal del Programa -----------
+while not hecho:
+    # TODOS LOS EVENTOS DE PROCESAMIENTO DEBERÍAN IR DEBAJO DE ESTE COMENTARIO
+    for evento in pygame.event.get(): # El usuario hizo algo
+        if evento.type == pygame.QUIT: # Si el usuario pincha sobre cerrar
+            hecho = True # Marca que indica que hemos acabado y sale de este bucle
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            pass
+        if evento.type==pygame.KEYDOWN:
+            if evento.key==pygame.K_UP:
+                
+                velocidad_y=-2
+            if evento.key==pygame.K_DOWN:
+                velocidad_y=2
+            if evento.key==pygame.K_RIGHT:
+                velocidad_x=2
+            if evento.key==pygame.K_LEFT:
+                velocidad_x=-2
+        if evento.type==pygame.KEYUP:
+            if evento.key==pygame.K_UP:
+                velocidad_y=0
+            if evento.key==pygame.K_DOWN:
+                velocidad_y=0
+            if evento.key==pygame.K_RIGHT:
+                velocidad_x=0
+            if evento.key==pygame.K_LEFT:
+                velocidad_x=0
+            
+    # TODOS LOS EVENTOS DE PROCESAMIENTO DEBERÍAN IR ENCIMA DE ESTE COMENTARIO
+    
+    
+    # TODA LA LÓGICA DEL JUEGO DEBERÍA IR DEBAJO DE ESTE COMENTARIO
+    
+    # TODA LA LÓGICA DEL JUEGO DEBERÍA IR ENCIMA DE ESTE COMENTARIO
+
+    x+=velocidad_x
+    y+=velocidad_y
+    
+    pos = pygame.mouse.get_pos()
+    xm = pos[0]
+    ym = pos[1]
+    
+    # TODO EL CÓDIGO DE DIBUJO DEBERÍA IR DEBAJO DE ESTE COMENTARIO
+   
+    
+    # Primero limpiamos pantalla. No dibujes por encima de esta linea
+    # o todo lo que escribas sera borrado por este comando.
+    pantalla.fill(BLANCO)
+    dibujar_fondo()
+    # pantalla.blit(pasto, [x, y])
+    pantalla.blit(principal, [xm, ym])
+    
+    
+    # DIBUJEMOS ALGUNAS FIGURAS
+    # barquito(x-200,y-200)
+    
+    # Avanza y actualiza la pantalla con lo que hemos dibujado.
+    pygame.display.flip()  
+    
+    # TODO EL CÓDIGO DE DIBUJO DEBERÍA IR ENCIMA DE ESTE COMENTARIO
+    
+    
+    # Limita a 20 fotogramas por segundo (frames per second)
+    reloj.tick(20)
+
+# Cierra la ventana.
+# Si olvidas poner esta linea el programa se 'colgara'.
+pygame.quit()
